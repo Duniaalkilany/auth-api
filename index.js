20cc66586c11ff465a7b3500d3fbd3571a66e19f
Dunia alkilany <82367199+Duniaalkilany@users.noreply.github.com>
2021-08-18 02:40:07 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 'use strict';
+// 3rd party modules
+require('dotenv').config()
 
-require('dotenv').config();
-const app = require('./src/server.js');
-const { db } = require('./src/models');
+// internal modules
+const server = require('./src/server');
+const {db} = require('./src/auth/models/index'); //destructuring es6
 
-db.sync().then(() => {
-  app.start(process.env.PORT || 3000);
-});
+
+db.sync().then(()=> {
+    server.start(process.env.PORT|| 3000);
+})
+.catch(console.error);
