afcbd33cc42980c75de093cc28864eb8dee9ab34
Dunia alkilany <82367199+Duniaalkilany@users.noreply.github.com>
2021-08-18 01:46:58 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ const app = require('./src/server.js');
 const { db } = require('./src/models');
 
 db.sync().then(() => {
-  app.start(process.env.PORT || 3001);
+  app.start(process.env.PORT || 3000);
 });
