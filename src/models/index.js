5112f4f049de0022d2437590c58f9eaa23496b27
Dunia alkilany <82367199+Duniaalkilany@users.noreply.github.com>
2021-08-18 14:25:06 +0300

    Update index.js

--- src/models/index.js
+++ src/models/index.js
@@ -8,10 +8,22 @@ const foodModel = require('../models/food/model.js');
 const userModel = require('./users');
 const Collection = require('./data-collection.js');
 
-const DATABASE_URL = process.env.NODE_ENV === 'test' ? 'sqlite:memory:' : process.env.DATABASE_URL||'postgres://localhost:5432/dunia';
-// const DATABASE_URL = process.env.DATABASE_URL 
+// const DATABASE_URL = process.env.NODE_ENV === 'test' ? 'sqlite:memory:' : process.env.DATABASE_URL||'postgres://localhost:5432/dunia';
+const DATABASE_URL = process.env.DATABASE_URL 
 
-const sequelize = new Sequelize(DATABASE_URL);
+let sequelizeOptions = {
+  dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      }
+    }
+};
+
+
+const sequelize = new Sequelize(DATABASE_URL, sequelizeOptions);
+
+
 const food = foodModel(sequelize, DataTypes);
 const clothes = clothesModel(sequelize, DataTypes);
 
